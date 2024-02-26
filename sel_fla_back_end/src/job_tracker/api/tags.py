c0833b548fcee7b0ab44ce60e6276be673ed7eb2
programB <35240807+programB@users.noreply.github.com>
2024-02-26 20:59:49 +0100

    (backend) implement tags endpoint plus tests

--- sel_fla_back_end/src/job_tracker/api/tags.py
+++ sel_fla_back_end/src/job_tracker/api/tags.py
@@ -1,6 +1,9 @@
-from . import mydb
+from job_tracker.models import Tag
+
+# from job_tracker.models import Tag, tags_schema
 
 
 def get_all():
-    tags = mydb.db_get_tags()
-    return tags
+    tags = Tag.query.all()
+    # return tags_schema.dump(tags)
+    return [tag.name for tag in tags]
---
+++ sel_fla_back_end/test/integration/api/test_tags.py
@@ -0,0 +1,19 @@
+import pytest
+
+
+class TestHappyPaths:
+    def test_should_get_all_existing_tags(self, httpx_test_client):
+        response = httpx_test_client.get("/api/tags")
+        assert response.status_code == 200
+        received_tags = response.json()
+
+        assert isinstance(received_tags, list) and all(
+            isinstance(item, str) for item in received_tags
+        )
+        assert len(received_tags) == 3
+        assert all(tag in ["Java", "Selenium", "Python"] for tag in received_tags)
+
+
+@pytest.mark.xfail(reason="communication with selenium grid not yet implemented.")
+def test_should_get_500error_if_server_not_healthy(httpx_test_client):
+    assert False
