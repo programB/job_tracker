e90fe90fc2f260f385d45fb1c46b42c02303b036
programB <35240807+programB@users.noreply.github.com>
2024-02-26 14:38:15 +0100

    (backend) implement offers endpoint

--- sel_fla_back_end/src/job_tracker/api/offers.py
+++ sel_fla_back_end/src/job_tracker/api/offers.py
@@ -1,6 +1,10 @@
-from . import mydb
+from flask import request
+
+from job_tracker.models import JobOffer, joboffers_schema
 
 
 def get_all():
-    offers = mydb.db_get_offers()
-    return offers
+    subpage = request.args.get("subpage", default=1, type=int)
+    perpagelimit = request.args.get("perpagelimit", type=int)
+    paginated_offers = JobOffer.query.paginate(page=subpage, per_page=perpagelimit)
+    return joboffers_schema.dump(paginated_offers)
