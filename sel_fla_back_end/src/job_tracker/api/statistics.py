6311d99288e918379b73d43f9a5912c27f15b7fa
programB <35240807+programB@users.noreply.github.com>
2024-03-06 16:56:15 +0100

    (backend) Move helper functions to separate module

--- sel_fla_back_end/src/job_tracker/api/statistics.py
+++ sel_fla_back_end/src/job_tracker/api/statistics.py
@@ -1,6 +1,5 @@
 from contextlib import contextmanager
 from datetime import datetime, timedelta
-from enum import Enum
 
 from connexion.problem import problem
 from flask import request
@@ -9,81 +8,16 @@ from sqlalchemy import and_, delete, func, inspect, select, true
 from job_tracker.database import db
 from job_tracker.models import JobOffer, datapoints_schema
 
+from .date_helpers import (
+    Interval,
+    ISO8601_date_type,
+    interval_type,
+    iterate_months,
+    last_day_of_month,
+)
+
 # Silence litner for lines using func (eg. func.extract or func.count)
 # pylint: disable=not-callable
-
-
-class Interval(Enum):
-    """Finite choice of binning periods"""
-
-    DAY = "day"
-    MONTH = "month"
-    YEAR = "year"
-
-
-def interval_type(interval: str) -> Interval:
-    match interval:
-        case "year":
-            return Interval.YEAR
-        case "month":
-            return Interval.MONTH
-        case "day":
-            return Interval.DAY
-        case _:
-            raise ValueError(
-                f"binning can only be one of: year, month, day - is: {interval}"
-            )
-
-
-def ISO8601_date_type(date: str) -> datetime:
-    """Attempts to create datetime object from date
-
-    If date string conforms to the YYYY-MM-DD format (RFC 3339)
-    it will be converted to datetime object with time set to 00:00:00
-
-    Parameters
-    ----------
-    date : str
-
-    Returns
-    -------
-    datetime
-
-    Raises
-    ------
-    ValueError
-    For date strings not conforming to the YYYY-MM-DD format
-    """
-
-    ISO8601_date_format = "%Y-%m-%d"
-    try:
-        return datetime.strptime(date, ISO8601_date_format)
-    except ValueError as e:
-        raise ValueError(
-            (
-                f"input passed ({date}) does not conform to RFC 3339, "
-                "it should be: YYYY-MM-DD"
-            )
-        ) from e
-
-
-def iterate_months(start_date, end_date):
-    """Yields timestamp of 1st day of every month between start and end dates
-
-    Time is always set to 00:00:00
-    Range includes both start_date and end_date
-    """
-    year = start_date.year
-    month = start_date.month
-    while True:
-        current = datetime(year, month, 1)
-        yield current
-        if current.month == end_date.month and current.year == end_date.year:
-            break
-        else:
-            month = ((month + 1) % 12) or 12
-            if month == 1:
-                year += 1
 
 
 @contextmanager
@@ -163,13 +97,6 @@ def calculate_stats(
     -------
     [(date, int)]
     """
-
-    def last_day_of_month(any_date: datetime) -> datetime:
-        # The day 28 exists in every month. 4 days later, it's always next month.
-        date_in_next_month = any_date.replace(day=28) + timedelta(days=4)
-        # Subtracting the number of the 'new' current day brings us back
-        # to the last day of the previous month.
-        return date_in_next_month - timedelta(days=date_in_next_month.day)
 
     with temporary_timestamps_table() as TmpContinuousDatesRange:
         # Expand date range (to full years or months)
