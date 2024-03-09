f11433f42941e985d0c2558e8bdc1f1af316c893
programB <35240807+programB@users.noreply.github.com>
2024-03-09 20:34:47 +0100

    (backend) add task getting offers from Pracuj.pl

--- sel_fla_back_end/src/job_tracker/tasks.py
+++ sel_fla_back_end/src/job_tracker/tasks.py
@@ -1,11 +1,18 @@
 import logging
 import string
+from contextlib import contextmanager
 from datetime import datetime
 from random import choices, randint
 
+from selenium import webdriver
+
+from job_tracker.config import root_dir
 from job_tracker.database import db
 from job_tracker.extensions import scheduler
 from job_tracker.models import Company
+from job_tracker.pracujpl_POM import Distance, PracujplMainPage, ResultsPage
+
+datafile = root_dir.joinpath("search_results.txt")
 
 
 @scheduler.task("interval", id="demo_task", seconds=4)
@@ -49,3 +56,60 @@ def put_some_data_into_db():
         )
         db.session.add(fake_company)
         db.session.commit()  # Flask-APScheduler requires this gets called
+
+
+@contextmanager
+def selenium_driver(selenium_grid_url, selenium_grid_port="4444"):
+
+    custom_options = webdriver.ChromeOptions()
+
+    if selenium_grid_url:
+        driver = webdriver.Remote(
+            command_executor=selenium_grid_url + ":" + selenium_grid_port,
+            options=custom_options,
+        )
+    else:
+        # Use local driver (local browser)
+        # if no Selenium Grid server url was given
+        driver = webdriver.Chrome(options=custom_options)
+
+    yield driver
+
+    driver.quit()
+
+
+@scheduler.task(
+    trigger="cron",
+    id="fetch_offers_task",
+    minute="*/5",
+    max_instances=1,
+    misfire_grace_time=30,  # seconds
+)
+def fetch_offers():
+
+    with selenium_driver(None) as driver:
+        main_page = PracujplMainPage(driver, reject_cookies=True)
+
+        if main_page.search_mode == "default":
+            main_page.search_mode = "it"
+        tags = main_page.search_mode == "it"
+        main_page.employment_type = ["full_time"]
+        main_page.location_and_distance = ("Warszawa", Distance.TEN_KM)
+        main_page.search_term = "Tester"
+
+        main_page.start_searching()
+
+        results_page = ResultsPage(driver)
+        all_offers = results_page.all_offers
+
+        with datafile.open("a", encoding="utf-8") as datf:
+            for i, offer in enumerate(all_offers):
+                datf.write(f"({i})  Offer: {offer.title} (id: {offer.id})\n")
+                datf.write(f" company:  {offer.company_name}\n")
+                datf.write(f" salary:   {offer.salary}\n")
+                datf.write(f" level:    {offer.job_level}\n")
+                datf.write(f" contract: {offer.contract_type}\n")
+                datf.write(f" link:     {offer.link}\n")
+                if tags:
+                    datf.write(f" tags:     {offer.technology_tags}\n")
+                datf.write("----------------------------\n")
