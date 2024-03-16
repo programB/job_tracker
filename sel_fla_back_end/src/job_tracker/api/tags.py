9de98f4c8df6f677c5d4bd85df06fa40bd9d2a82
programB <35240807+programB@users.noreply.github.com>
2024-03-16 16:21:44 +0100

    (backend) handle db connection loss in tags endpoint

--- sel_fla_back_end/src/job_tracker/api/tags.py
+++ sel_fla_back_end/src/job_tracker/api/tags.py
@@ -1,9 +1,25 @@
+import logging
+
+from connexion.problem import problem
+from sqlalchemy import exc
+
 from job_tracker.models import Tag
 
 # from job_tracker.models import Tag, tags_schema
 
+logger = logging.getLogger(__name__)
+
 
 def get_all():
-    tags = Tag.query.all()
-    # return tags_schema.dump(tags)
-    return [tag.name for tag in tags]
+    try:
+        tags = Tag.query.all()
+    except exc.OperationalError:
+        logger.exception(
+            ("Failed to connect to the database while trying query for tags")
+        )
+        return problem(
+            status=500, title="database offline", detail="Check server health"
+        )
+    else:
+        # return tags_schema.dump(tags)
+        return [tag.name for tag in tags]
