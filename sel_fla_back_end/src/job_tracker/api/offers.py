6c50833be2ee0abe7d1543c37dc304109deb34cf
programB <35240807+programB@users.noreply.github.com>
2024-03-16 16:10:46 +0100

    (backend) handle db connection loss in offers endpoint

--- sel_fla_back_end/src/job_tracker/api/offers.py
+++ sel_fla_back_end/src/job_tracker/api/offers.py
@@ -1,10 +1,25 @@
+import logging
+
+from connexion.problem import problem
 from flask import request
+from sqlalchemy import exc
 
 from job_tracker.models import JobOffer, joboffers_schema
+
+logger = logging.getLogger(__name__)
 
 
 def get_all():
     subpage = request.args.get("subpage", default=1, type=int)
     perpagelimit = request.args.get("perpagelimit", type=int)
-    paginated_offers = JobOffer.query.paginate(page=subpage, per_page=perpagelimit)
-    return joboffers_schema.dump(paginated_offers)
+    try:
+        paginated_offers = JobOffer.query.paginate(page=subpage, per_page=perpagelimit)
+    except exc.OperationalError:
+        logger.exception(
+            ("Failed to connect to the database while trying query for offers")
+        )
+        return problem(
+            status=500, title="database offline", detail="Check server health"
+        )
+    else:
+        return joboffers_schema.dump(paginated_offers)
