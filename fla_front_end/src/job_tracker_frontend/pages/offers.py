805c4ae238f5dd17e7aeb5f931786856e32c1716
programB <35240807+programB@users.noreply.github.com>
2024-03-30 16:03:07 +0100

    (frontend) implement pagination on offers route

--- fla_front_end/src/job_tracker_frontend/pages/offers.py
+++ fla_front_end/src/job_tracker_frontend/pages/offers.py
@@ -1,4 +1,5 @@
 from flask import render_template
+from flask import  request
 
 from job_tracker_frontend.backend_comm.exceptions import (
     APIException,
@@ -10,14 +11,23 @@ from job_tracker_frontend.pages import bp
 
 @bp.route("/offers")
 def offers():
+    # hard-coded for now
+    # See backend api for other accpeted values
+    offers_per_subpage = 10
     try:
-        offs = get_offers(10, 1)
+        subpage = request.args.get("subpage", default=1, type=int)
+        ans = get_offers(offers_per_subpage, subpage)
     except (AttributeError, APIException, BackendNotAvailableException):
         print("Could not access backend service")
         return "<p> error while trying to communicate with backend service</p>"
 
-    for off in offs:
+    for off in ans["offers"]:
         # Return just the date (without time)
         off["collected"] = off["collected"].split("T")[0]
         off["posted"] = off["posted"].split("T")[0]
-    return render_template("pages/offers.html", offs=offs)
+    return render_template(
+        "pages/offers.html",
+        tot_subpages=ans["info"]["tot_subpages"],
+        curr_subpage=ans["info"]["curr_subpage"],
+        offs=ans["offers"],
+    )
