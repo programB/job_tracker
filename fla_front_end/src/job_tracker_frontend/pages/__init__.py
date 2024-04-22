2ca3e1cd00130e009baef8489be74f7d10996507
programB <35240807+programB@users.noreply.github.com>
2024-04-22 15:55:12 +0200

    (frontend) remove the About page

--- fla_front_end/src/job_tracker_frontend/pages/__init__.py
+++ fla_front_end/src/job_tracker_frontend/pages/__init__.py
@@ -2,7 +2,7 @@ from flask import Blueprint
 
 bp = Blueprint("pages", __name__)
 
-from job_tracker_frontend.pages import about, home, offers, status
+from job_tracker_frontend.pages import home, offers, status
 
 # Statistics page is provided by
 # the statistics_dash sub module
